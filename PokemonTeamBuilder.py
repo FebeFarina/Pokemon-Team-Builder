53c50a0b7681d0515a9f3b6eeae7722de6d6a6b2
FebeFarina <alu0101430973@ull.edu.es>
2022-12-16 16:35:20 +0000

    Ventana sin funcionar

--- PokemonTeamBuilder.py
+++ PokemonTeamBuilder.py
@@ -1,16 +1,44 @@
+import matplotlib
+matplotlib.use("Agg")
+
 from pyswip import Prolog
+import sys
+import os
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Pokemon Team Builder")
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
 prolog = Prolog()
 prolog.consult("src/pokemon_rules.pl")
 team = []
+team_weaknesses = []
+team_resistances = []
 while len(team) < 6:
-    weak_list = []
     pokemon = input("Enter a pokemon: ")
     if pokemon == "quit":
         break
     team.append(pokemon)
-    for weakness in prolog.query("weak_against("+pokemon+",X)"):
-        if weakness['X'] not in weak_list:
-            weak_list.append(weakness['X'])
-    print("{} is weak against: {}.".format(pokemon, weak_list))
 print("Your team is: {}.".format(team))
+text = ', '.join(team)
+text = f"[{text}]"
+for matchup in prolog.query("teamResistsAndWeaknesses("+text+",R,W)"):
+    team_weaknesses.append(matchup['W'])
+    team_resistances.append(matchup['R'])
+full_team_weak_list = [item for sublist in team_weaknesses for item in sublist]
+full_team_resist_list = [item for sublist in team_resistances for item in sublist]
+full_team_weak_list = list(dict.fromkeys(full_team_weak_list))
+full_team_resist_list = list(dict.fromkeys(full_team_resist_list))
+for types in full_team_weak_list:
+    if types in full_team_resist_list:
+        full_team_weak_list.remove(types)
+        full_team_resist_list.remove(types)
+print("Your team is weak against: {}.".format(full_team_weak_list))
+print("Your team is resistant against: {}.".format(full_team_resist_list))
+
+
--- src/pokemon_rules.pl
+++ src/pokemon_rules.pl
@@ -29,30 +29,17 @@ strong_against(P, X):-
     have_type(P, T),
     super_effective(T, X).
 
-cWR(R, W, L):-
-    type(R),
-    \+ member(R, W),
-    append([], [R], L).
-cWR(_R, _W, []).
-
-cancelWeaknessResists([], _W, []).
-cancelWeaknessResists([R|TR], W, L) :-
-  is_list(TR),
-  is_list(W),
-  cancelWeaknessResists(TR, W, TailL),
-  cWR(R, W, WRList),
-  append(WRList, TailL, L).
-
 pokemonResistsAndWeaknesses([], [], []).
 pokemonResistsAndWeaknesses(T, R, W) :-
     type(T),
+    !,
     findall(X, weak(T, X), W),
     findall(X, resistant(T, X), R),
-    cancelWeaknessResists(R, W, L).
+    !.
 pokemonResistsAndWeaknesses(P, R, W) :-
   pokemon(P),
-  have_type(P, T),
   !,
+  have_type(P, T),
   pokemonResistsAndWeaknesses(T, R, W).
 
 /* Reglas de equipos */
@@ -71,24 +58,3 @@ teamResistsAndWeaknesses(T, R, W) :-
   teamResistsAndWeaknesses(B, TR, TW),
   append(PR, TR, R),
   append(PW, TW, W).
-
-teamTypeCovered(T) :-
-  teamResistsAndWeaknesses(T, R, _W),
-  member(normal, R),
-  member(fire, R),
-  member(water, R),
-  member(electric, R),
-  member(grass, R),
-  member(ice, R),
-  member(fighting, R),
-  member(poison, R),
-  member(ground, R),
-  member(flying, R),
-  member(psychic, R),
-  member(bug, R),
-  member(rock, R),
-  member(ghost, R),
-  member(dragon, R),
-  member(dark, R),
-  member(steel, R),
-  member(fairy, R).
